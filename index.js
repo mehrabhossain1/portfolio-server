28831a4cb0f202514e85566469c117ca79839dad
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-05-13 17:26:10 +0600

    origin: *

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // MongoDB Connection URL
